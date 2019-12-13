b73072d8066f0a1ec5ff919f2aef52c0c198dc2e
DFaustino6 <a61171@ualg.pt>
2019-12-13 23:00:42 +0000

    start

---
+++ resources/views/home_template.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<hmtl>
+<head>
+  <title>FORUM</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <style>
+        .carousel-inner img {
+            margin: auto;
+        }
+  </style>
+</head>
+<body>
+
+</body>
+</html>
