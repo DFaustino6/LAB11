bc4bcc4f7d5be2d7e30a10028b37ea95a3da561a
DFaustino6 <a61171@ualg.pt>
2019-12-15 00:55:34 +0000

    menu

--- resources/views/navbar_template.blade.php
+++ resources/views/navbar_template.blade.php
@@ -1,8 +1,40 @@
-
+<link rel="stylesheet" href="{{asset('resources/assets/css/megamenu.css')}}">
 <nav class="navbar  navbar-dark bg-dark navbar-expand" style="margin-top: 0.1%">
     <img class="align-self-center mr-2" src="resources\assets\images\logo.jpg" alt="Generic placeholder image" style="width: 2%;height: 2%">
     <a class="navbar-brand" href="#"><h3>Óculos Online</h3></a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <a href="#" class="navbar-brand font-weight-bold d-block d-lg-none">MegaMenu</a>
+  	<button type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbars" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+  <div id="navbarContent" class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <!-- Megamenu-->
+      <li class="nav-item dropdown megamenu"><a id="megamneu" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle font-weight-bold text-uppercase">Categorias</a>
+        <div aria-labelledby="megamneu" class="dropdown-menu">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-6">
+                <h6 class="font-weight-bold text-uppercase">Óculos de Sol</h6>
+                <ul class="list-unstyled">
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Homem</a></li>
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Mulher</a></li>
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Criança</a></li>
+                </ul>
+              </div> 
+              <div class="col-6 mr-3">
+                <h6 class="font-weight-bold text-uppercase">Óculos de Sol</h6>
+                <ul class="list-unstyled">
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Homem</a></li>
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Mulher</a></li>
+                  <li class="nav-item"><a href="" class="nav-link text-small" style="color: black">Criança</a></li>
+                </ul>
+              </div>   
+            </div>  
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
         <a class="nav-link" href="#"><h5 style="color: white">Login</h5></a>
