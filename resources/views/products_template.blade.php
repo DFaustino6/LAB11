1525740c6709ad2439d194d432fedc19a8981cd2
DFaustino6 <a61171@ualg.pt>
2019-12-16 19:10:55 +0000

    products update

--- resources/views/products_template.blade.php
+++ resources/views/products_template.blade.php
@@ -1,44 +1,14 @@
 <div class="container-fluid">
-	<div class="row mt-3">
-		<div class="col-3 text-center">
-			<div class="card mx-auto border-dark" style="width: 400px">			
-				<img class="mx-auto d-block" src="resources\assets\images\oculos1.jpg" style="width: 200px;height: 200px;">
-				<div class="card-body">
-					<h5 class="card-title">Oculos Rayban</h5>
-					<h5 class="card-title">100€</h5>
-					<a href="#" class="btn btn-primary btn-lg active mx-auto " role="button" aria-pressed="true">Adicionar ao Carrinho</a>
+		<div class="row mt-3">
+			<div class="col-3 text-center">
+				<div class="card mx-auto border-dark" style="width: 400px">			
+					<img class="mx-auto d-block" src="resources\assets\images\oculos1.jpg" style="width: 200px;height: 200px;">
+					<div class="card-body">
+						<h5 class="card-title">Oculos Rayban</h5>
+						<h5 class="card-title">100€</h5>
+						<a href="#" class="btn btn-primary btn-lg active mx-auto " role="button" aria-pressed="true">Adicionar ao Carrinho</a>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-3 text-center">
-			<div class="card mx-auto border-dark" style="width: 400px">	
-				<img class="mx-auto d-block" src="resources\assets\images\oculos1.jpg" style="width: 200px;height: 200px;">
-				<div class="card-body">
-					<h5 class="card-title">Oculos Rayban</h5>
-					<h5 class="card-title">100€</h5>
-					<a href="#" class="btn btn-primary btn-lg active mx-auto " role="button" aria-pressed="true">Adicionar ao Carrinho</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-3 text-center">
-			<div class="card mx-auto border-dark" style="width: 400px">
-				<img class="mx-auto d-block" src="resources\assets\images\oculos1.jpg" style="width: 200px;height: 200px;">
-				<div class="card-body">
-					<h5 class="card-title">Oculos Rayban</h5>
-					<h5 class="card-title">100€</h5>
-					<a href="#" class="btn btn-primary btn-lg active mx-auto " role="button" aria-pressed="true">Adicionar ao Carrinho</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-3 text-center">
-			<div class="card mx-auto border-dark" style="width: 400px">				
-				<img class="mx-auto d-block" src="resources\assets\images\oculos1.jpg" style="width: 200px;height: 200px;">
-				<div class="card-body">
-					<h5 class="card-title">Oculos Rayban</h5>
-					<h5 class="card-title">100€</h5>
-					<a href="#" class="btn btn-primary btn-lg active mx-auto " role="button" aria-pressed="true">Adicionar ao Carrinho</a>
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
